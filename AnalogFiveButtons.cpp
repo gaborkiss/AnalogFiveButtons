2d8ba222c15f00106b91a0e0eaed6cde6903cbf4
Gábor <exon@freemail.hu>
2015-04-16 20:04:44 +0200

    Update AnalogFiveButtons.cpp

--- AnalogFiveButtons.cpp
+++ AnalogFiveButtons.cpp
@@ -1,8 +1,18 @@
 /*
-  AnalogFiveButtons class for the analog 5 keys panels
-  Version 1.0
-  2015 - DataMiner.hu, Gábor Kiss
-  V 1.0
+ AnalogFiveButtons class for the analog 5 keys panels
+ Version 1.0
+
+ Copyright (c) 2015 - Kiss Gábor (kiss-gabor@outlook.hu)
+
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+ 
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
 */
 
 
