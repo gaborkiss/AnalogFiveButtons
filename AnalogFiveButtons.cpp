235328f87eb6b16fa01ea944d42d3d7774ae1004
Gábor <exon@freemail.hu>
2015-04-16 19:11:10 +0200

    Create AnalogFiveButtons.cpp

---
+++ AnalogFiveButtons.cpp
@@ -0,0 +1,99 @@
+/*
+  AnalogFiveButtons class for the analog 5 keys panels
+  Version 1.0
+  2015 - DataMiner.hu, Gábor Kiss
+*/
+
+
+#include <inttypes.h>
+#include <Arduino.h>
+#include "AnalogFiveButtons.h"
+
+
+#define DEFAULT_FAILURE_RATE		10
+
+
+
+// analogPin : where is the keypad OUT pin connected (A0, A1..)
+// typeOfModule: 0-red Keyes_AD_Key module, 1-LCD Keypad module
+AnalogFiveButtons::AnalogFiveButtons(uint8_t analogPin, uint8_t typeOfModule)
+{
+	if(typeOfModule == KEYES_AD_KEY) {
+		
+		// Default values for the Keyes_AD_Key module
+		// Parameters: analog pin, failure rate,                          left, up,  down, right, fire
+		init(                 analogPin, DEFAULT_FAILURE_RATE, 0,   140, 320,   500,  740);
+		
+	}  else if(typeOfModule == LCD_KEYPAD) {
+		
+		// Default values for the LCD Keypad module
+		// Parameters: analog pin, failure rate, 		                left,  up,   down, right, fire
+		init(				  analogPin, DEFAULT_FAILURE_RATE, 480, 130, 305,   0,      720);	
+	}
+}
+
+
+
+// Custom defined module
+// Parameters: analogue pin, failure rate, left, up, down, right, fire values
+AnalogFiveButtons::AnalogFiveButtons(uint8_t analogPin, uint8_t failureRate, int sw1Value, int sw2Value, int sw3Value, int sw4Value, int sw5Value)
+{
+	init(analogPin, failureRate, sw1Value, sw2Value, sw3Value, sw4Value, sw5Value);
+}
+
+
+
+void AnalogFiveButtons::init(uint8_t analogPin, uint8_t failureRate, int sw1Value, int sw2Value, int sw3Value, int sw4Value, int sw5Value)
+{
+	analogReference(DEFAULT);
+	
+	_analogPin = analogPin;
+	_failureRate = failureRate;
+	_sw1Value = sw1Value; 
+	_sw2Value = sw2Value; 
+	_sw3Value = sw3Value; 
+	_sw4Value = sw4Value; 
+	_sw5Value = sw5Value; 
+    sensorValue = 0;
+    oldKey = 1;
+    newKey = 0;
+}
+
+
+void AnalogFiveButtons::evalButton()
+{
+  // Key reset
+  newKey = SW_NO_PUSH;
+  
+  sensorValue = analogRead(_analogPin);
+  
+  if( (sensorValue > (_sw5Value - _failureRate)) && (sensorValue < (_sw5Value + _failureRate)) )		newKey = 5;  // Fire
+  if( (sensorValue > (_sw4Value - _failureRate)) && (sensorValue < (_sw4Value + _failureRate)) )		newKey = 4;  // Right
+  if( (sensorValue > (_sw3Value - _failureRate)) && (sensorValue < (_sw3Value + _failureRate)) )		newKey = 3;  // Down
+  if( (sensorValue > (_sw2Value - _failureRate)) && (sensorValue < (_sw2Value + _failureRate)) )		newKey = 2;  // Up
+  if( (sensorValue > (_sw1Value - _failureRate)) && (sensorValue < (_sw1Value + _failureRate)) )		newKey = 1;  // Left
+}
+
+
+uint8_t AnalogFiveButtons::getButton()
+{
+  evalButton();
+  
+  return newKey;
+}
+
+
+uint8_t AnalogFiveButtons::changedButton()
+{
+  evalButton();
+  
+  if(oldKey == newKey)
+  {
+    // Nothing is changed
+    newKey = SW_NO_CHANGE;
+  } else {
+    oldKey = newKey;
+  }
+  
+  return newKey;
+}
